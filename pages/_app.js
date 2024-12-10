d28426751d8e23ac791d5105d5705372c75139ea
Gurjeet Singh Virdee <73753957+gurjeetsinghvirdee@users.noreply.github.com>
2024-12-10 18:59:32 +0530

    Integrated Next.js for client-side Rendering

---
+++ pages/_app.js
@@ -0,0 +1,14 @@
+import Head from 'next/head';
+
+function HomePage() {
+    return (
+        <div>
+            <Head>
+                <title>RecoGenie</title>
+            </Head>
+            <h1>Welcome to RecoGenie</h1>
+        </div>
+    );
+}
+
+export default HomePage;
